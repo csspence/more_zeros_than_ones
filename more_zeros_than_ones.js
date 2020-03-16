8a65a48dec1dd438989d2c76e452a3a81c36764f
Crew Spence <csspence123@gmail.com>
2020-03-16 07:32:33 -0500

    Wrote naive solution for more_zeros_than_ones.

--- more_zeros_than_ones.js
+++ more_zeros_than_ones.js
@@ -16,6 +16,40 @@ Examples
 All input will be valid strings of length > 0.
 */
 
-function moreZeros(s){
+const moreZeros = (s) => {
+  const asciiObj = {};
+  const helperArr = [];
+  const returnArr = [];
   
+  //Fills helperArr without duplicates
+  for(let i = 0; i < s.length; i++) {
+    if(asciiObj[s[i]] === undefined) {
+      asciiObj[s[i]] = true;
+      helperArr.push(s[i]);
+    }
+  }
+
+  const sumOnesAndZeros = (char) => {
+    let ones = 0;
+    let zeros = 0;
+    let string = char.charCodeAt().toString(2);
+    for(let j = 0; j < string.length; j++) {
+      if(string[j] === '1') {
+        ones++;
+      } else {
+        zeros++;
+      }
+    }
+
+    return (zeros > ones ? true : false);
+  }
+
+  //Fills returnArr
+  for(let h = 0; h < helperArr.length; h++) {
+    if(sumOnesAndZeros(helperArr[h]) === true) {
+      returnArr.push(helperArr[h]);
+    }
+  }
+
+  return returnArr;
 }
